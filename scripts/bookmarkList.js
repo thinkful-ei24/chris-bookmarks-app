fe7dba0ec80f2aa4a4c85f988f54829c0b23705c
Chris Rudder <cbrudder84@gmail.com>
2018-08-30 23:17:33 -0400

    worked on rendering DOM

--- scripts/bookmarkList.js
+++ scripts/bookmarkList.js
@@ -17,7 +17,22 @@ const bookmarkList = (function(){
       </section>
     `;
   }
+  function render(){
+    store.items.forEach(item => {
+      const str = `
+      <li class="js-item-element" data-item-id="${item.id}">
+        ${item.title}
+        <button class="bookmark-list-expand">Expand Button</button>
 
+        <div>
+          <p>${item.desc}</p>
+          <button class="bookmark-list-delete" data-item-id="${item.id}">Delete Bookmark</button>
+          <a href="${item.url}">Go to ${item.title}</a
+        </div>
+      </li>
+      `;
+    });
+  }
   // base for generateBookmarkListString finished
   // function generateBookmarkListString(bookmarkList) {
   //   const items = bookmarkList.map((item) => generateBaseHtml(item));
@@ -34,23 +49,30 @@ const bookmarkList = (function(){
   //   }
 
   //sort by stars
+ 
+  $('.star').click((element) => {
+    const rating = element.target.getAttribute('data-item-id');
+    $('#rating').attr('value', rating);
+  });
+  
     
   function handleNewItemSubmit() {
     $('#js-bookmark-list-form').submit(function (event) {
       event.preventDefault();
-        
+      const rating = $('#rating').val();
       const newTitle = $('.js-bookmark-list-title').val();
       const newUrl = $('.js-bookmark-list-url').val();
       const newDescription = $('.js-bookmark-list-description').val();
       
-      console.log(newTitle);
-      console.log(newUrl);
-      console.log(newDescription);
+      console.log(rating);
 
-      API.createItem(newTitle, newUrl, 5, newDescription,
+      
+      API.createItem(newTitle, newUrl, rating, newDescription,
         (newItem) => {
           store.addItem(newItem);
-        });
+          render();
+        }
+      );
     });
   }
 
